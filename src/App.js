6202c0a4b53f1476c611f7954551b5f8bc79cce7
Temially <temitayoalebiosu@gmail.com>
2020-05-04 01:13:32 +0100

    My firudemy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Home from './Home'
+import Instructor from './instructor'
+import Student from './Student'
+import Navigation from './Navbar'
+import {Container, Navbar, Row, Col} from 'react-bootstrap';
+import { BrowserRouter, Router, Switch, Route, Link, NavLink } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+
+
+    <BrowserRouter>
+    
+   <div>
+
+    <Navigation/>
+
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/student' component={Student}/>
+      <Route path='/instructor' component={Instructor}/>
+    </Switch>
+    
     </div>
+
+    </BrowserRouter>
+  
+    
   );
+  
 }
 
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './Home';
+import Contain from './Navbar';
+import Course from './course';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
